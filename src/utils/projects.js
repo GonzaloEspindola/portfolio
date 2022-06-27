0534169254180c98476ad125ebcc21bae910621c
Gonzalo Espindola <espindolamgonzalo@gmail.com>
2022-06-27 19:39:49 -0300

    add projects json

---
+++ src/utils/projects.js
@@ -0,0 +1,30 @@
+const projects = [{
+  title: 'Gestor de gastos',
+  subtitle: 'Gestor de gastos hecho en react, con CRUD persistiendo datos en base de datos postgresql a travez de API realizada por mi',
+  tech: ['html5', 'css3', 'js', 'react', 'node', 'git'],
+  imageLink: 'https://i.ibb.co/Swqf2XP/image.png',
+  githubLink: 'https://github.com/GonzaloEspindola/gestor-de-gastos',
+  demoLink: 'https://gonzaloespindola.github.io/gestor-de-gastos/login',
+  date: 'FullStack'
+},
+{
+  title: 'API de "Dragon Ball Super"',
+  subtitle: 'API de los personajes de la serie "Dragon ball" hecha con node.js',
+  tech: ['react', 'node', 'git', 'postgresql'],
+  imageLink: 'https://i.ibb.co/1Kq3cCk/dragon-ball-super-api-herokuapp-com-2.png',
+  githubLink: 'https://github.com/GonzaloEspindola/dbz_api_nodejs',
+  demoLink: 'dragon-ball-super-api.herokuapp.com/',
+  date: 'Backend'
+},
+{
+  title: 'League of Legends SPA',
+  subtitle: 'Single Web Page consumiendo la API oficial del juego League Of Legends"',
+  tech: ['html5', 'css3', 'js', 'react', 'git'],
+  imageLink: 'https://i.postimg.cc/tgQzSJ9H/gonzaloespindola-github-io-leagueoflegends-spa.png',
+  githubLink: 'https://github.com/GonzaloEspindola/leagueoflegends_spa',
+  demoLink: 'https://gonzaloespindola.github.io/leagueoflegends_spa/',
+  date: 'Frontend'
+}
+]
+
+export { projects }
